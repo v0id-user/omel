6bfebf4ac159b6c009ae12c43058ed444852aa31
#V0ID <hey@v0id.me>
2025-04-07 06:49:07 +0300

    chore(dependencies): update Next.js to version 15.3.0-canary.40 and add experimental nodeMiddleware option in next.config.ts; create initial SignIn component

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,3 @@
+export default function SignIn() {
+  return <div>TODO: SignIn</div>;
+}
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import { withSentryConfig } from '@sentry/nextjs';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    nodeMiddleware: true,
+  },
 };
 
 export default withSentryConfig(nextConfig, {
