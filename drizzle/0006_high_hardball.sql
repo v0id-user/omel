31cd0d85e66e4a788f9b7e8e6b7b8a90c27ee2c1
#V0ID <hey@v0id.me>
2025-04-24 04:01:09 +0300

    feat(popover): add Popover component

---
+++ drizzle/0006_high_hardball.sql
@@ -0,0 +1,9 @@
+ALTER TABLE "accounts" ALTER COLUMN "id" SET DEFAULT 'cxpr70vawi9slhqbc0elvv44';--> statement-breakpoint
+ALTER TABLE "rate_limits" ALTER COLUMN "id" SET DEFAULT 'k7xrz06w8vp8lw9lsjtomj69';--> statement-breakpoint
+ALTER TABLE "sessions" ALTER COLUMN "id" SET DEFAULT 'c4y8svq71vnjvc9ccj4ejewg';--> statement-breakpoint
+ALTER TABLE "two_factors" ALTER COLUMN "id" SET DEFAULT 'gwfvt6uc98qh4t0wslvxmro8';--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "id" SET DEFAULT 'gukg8nr51kh88r3q05ac0jmp';--> statement-breakpoint
+ALTER TABLE "verifications" ALTER COLUMN "id" SET DEFAULT 'ulh7wb05ztb3fbag7cd15wmk';--> statement-breakpoint
+ALTER TABLE "categories" ALTER COLUMN "id" SET DEFAULT 'bc3opdlh1s2c3qrt098yrk7e';--> statement-breakpoint
+ALTER TABLE "contacts" ALTER COLUMN "id" SET DEFAULT 'qozg71r9murmdisv3qpsdggn';--> statement-breakpoint
+ALTER TABLE "tasks" ALTER COLUMN "id" SET DEFAULT 'zzdu1xvrpx75bujv9zrkqamx';
