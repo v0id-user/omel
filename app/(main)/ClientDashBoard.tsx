82a6d2db71b144862fd81ba4b11b30a5be2778c1
#V0ID <hey@v0id.me>
2025-04-27 09:12:34 +0300

    refactor(ClientDashBoard): replace authClient session and organization management with userInfo store; streamline loading state and user data handling

--- app/(main)/ClientDashBoard.tsx
+++ app/(main)/ClientDashBoard.tsx
@@ -1,35 +1,28 @@
 'use client';
 
 import { DashboardSidebar } from '@/components/dashboard';
-import { authClient } from '@/lib/betterauth/auth-client';
+import { useUserInfoStore } from '@/store/persist/userInfo';
 import { useRouter } from 'next/navigation';
 export default function ClientDashBoard() {
-  const org = authClient.useActiveOrganization();
-  const session = authClient.useSession();
-  const router = useRouter();
-  const Spinner = () => (
-    <div className="fixed inset-0 bg-white z-50 flex items-center justify-center">
-      <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900"></div>
-    </div>
-  );
+  const userInfo = useUserInfoStore();
+  const info = userInfo.getUserInfo();
 
-  if (org.isPending || session.isPending) {
-    return <Spinner />;
-  }
-  if (!org.data || !session.data) {
+  const router = useRouter();
+
+  if (!info || !info.organizationInfo?.companyInfo?.name) {
     router.push('/clock-in');
-    return <Spinner />;
+    return <></>;
   }
 
   return (
     <DashboardSidebar
       variant="inset"
-      organizationName={org.data.name}
+      organizationName={info.organizationInfo.companyInfo.name}
       user={{
-        name: session.data.user.name,
-        email: session.data.user.email,
-        userId: session.data.user.id,
-        avatar: session.data.user.image || '',
+        name: `${info.personalInfo.firstName} ${info.personalInfo.lastName}`,
+        email: info.email,
+        userId: info.userId,
+        avatar: '',
       }}
     />
   );
