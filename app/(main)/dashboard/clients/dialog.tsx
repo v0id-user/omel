b6dae6ddaac1a80b8c7f2b010d90baaaf0ea3db1
#V0ID <hey@v0id.me>
2025-05-01 10:26:11 +0300

    feat(clients): add input fields foundation and submit button to the AddClientsDialog for enhanced

--- app/(main)/dashboard/clients/dialog.tsx
+++ app/(main)/dashboard/clients/dialog.tsx
@@ -1,5 +1,8 @@
 import { DashboardDialog } from '@/components/dashboard/Dialog';
 import { UserPlus } from 'iconoir-react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
 interface ClientsDialogProps {
   isOpen: boolean;
   onClose: () => void;
@@ -12,7 +15,16 @@ export function AddClientsDialog({ isOpen, onClose }: ClientsDialogProps) {
       title="إضافة عميل"
       icon={<UserPlus className="w-4 h-4" />}
     >
-      <div>
+      <div className="flex flex-col gap-4 mx-auto max-w-md">
+        <Input type="text" placeholder="الاسم" />
+        <Input type="text" placeholder="البريد الإلكتروني" />
+        <Input type="text" placeholder="رقم الهاتف" />
+        <Input type="text" placeholder="العنوان" />
+        <Input type="text" placeholder="المدينة" />
+        <Input type="text" placeholder="المنطقة" />
+        <Input type="text" placeholder="البلد" />
+        <Input type="text" placeholder="الرمز البريدي" />
+        <Button type="submit">إضافة</Button>
         <h1>TODO: Make the form to add new clients</h1>
       </div>
     </DashboardDialog>
